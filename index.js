82b0e50daa44ed26be33307e7df6ec80850e94f0
Software Waters <128394988+softwarewaters@users.noreply.github.com>
2025-12-16 11:52:56 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const {
     Client, GatewayIntentBits, Collection, REST, Routes, 
     SlashCommandBuilder, PermissionFlagsBits, ActivityType, 
     EmbedBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder, 
-    ChannelType 
+    ChannelType, ButtonBuilder, ButtonStyle 
 } = require('discord.js');
+
 const dotenv = require('dotenv');
 const fs = require('fs');
 const express = require('express');
